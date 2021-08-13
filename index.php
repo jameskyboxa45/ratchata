2197fdccfa2dcf843cb88d0d8fd772cb1df7548b
jameskyboxa45 <88876153+jameskyboxa45@users.noreply.github.com>
2021-08-13 14:33:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <br></br>
  <iframe src = https://thingspeak.com/channels/1458407/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
 <br></br>
-  <iframe src = https://thingspeak.com/channels/1458407/maps/channel_show  style="height:40%;width:35%;"></iframe>
+  <iframe src = https://thingspeak.com/channels/1458407/maps/channel_show  style="height:45%;width:35%;"></iframe>
 
 
