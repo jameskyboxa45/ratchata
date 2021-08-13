a6e9d30b464fa0bac3338f1120bcbfa6860b7e27
jameskyboxa45 <88876153+jameskyboxa45@users.noreply.github.com>
2021-08-13 14:18:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 <h1>Ratchata techapatamanon<h1>
   
 <iframe src = https://thingspeak.com/channels/1458407/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15 style="height:100%;width:100%;"></iframe>
+<iframe src = https://thingspeak.com/channels/1458407/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15 style="height:100%;width:100%;"></iframe>
