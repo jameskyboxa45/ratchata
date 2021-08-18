76bf223c3d1231d0780089637b85cf4d715026ff
jameskyboxa45 <88876153+jameskyboxa45@users.noreply.github.com>
2021-08-18 16:05:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 
+ <h1>Ratchata techapatamanon<h1>
 
+<iframe src = https://thingspeak.com/channels/1458407/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+  <br></br>
+ <iframe src = https://thingspeak.com/channels/1458407/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+<br></br>
+  <iframe src = https://thingspeak.com/channels/1458407/maps/channel_show  style="height:45%;width:35%;"></iframe>
 <html>
 <head>
 <script type="text/JavaScript">
@@ -11,13 +17,7 @@ function timedRefresh(timeoutPeriod) {
 </script>
 </head>
 <body onload="JavaScript:timedRefresh(5000);">
-  <h1>Ratchata techapatamanon<h1>
-
-<iframe src = https://thingspeak.com/channels/1458407/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
-  <br></br>
- <iframe src = https://thingspeak.com/channels/1458407/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
-<br></br>
-  <iframe src = https://thingspeak.com/channels/1458407/maps/channel_show  style="height:45%;width:35%;"></iframe>
+ 
 <?php
 
  $humidity = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/1/last.txt');
