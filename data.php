19be1af14f6add16597e09a9d332b924fcc809ab
jameskyboxa45 <88876153+jameskyboxa45@users.noreply.github.com>
2021-08-18 15:15:57 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,17 +1,1 @@
-<h1>Ratchata techapatamanon<h1>
-  
-<iframe src = https://thingspeak.com/channels/1458407/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
-  <br></br>
- <iframe src = https://thingspeak.com/channels/1458407/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
-<br></br>
-  <iframe src = https://thingspeak.com/channels/1458407/maps/channel_show  style="height:45%;width:35%;"></iframe>
-<?php
-
- $humidity = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/1/last.txt');
-$temp = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/2/last.txt');
-
- echo "temp is = ".$temp."<br>";
- echo "humidity is = ".$humidity;
-
-?>
 
