3c81344f2cb5d6102d09fdcb90f74079cf34650d
jameskyboxa45 <88876153+jameskyboxa45@users.noreply.github.com>
2021-08-18 14:17:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,15 @@
  <iframe src = https://thingspeak.com/channels/1458407/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
 <br></br>
   <iframe src = https://thingspeak.com/channels/1458407/maps/channel_show  style="height:45%;width:35%;"></iframe>
-GET https://api.thingspeak.com/channels/1458407/feeds.json?results=1
-GET https://api.thingspeak.com/channels/1458407/feeds.json?results=2
+<?php
+
+ $humidity = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/1/humanity.txt');
+$temp = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/2/temp.txt');
+
+ echo "temp is = ".$temp."<br>";
+ echo "humidity is = ".$humidity;
+
+?>
 
 
 
