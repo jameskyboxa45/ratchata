06659547e1053d3945573cf8e63979d6ff9120fb
jameskyboxa45 <88876153+jameskyboxa45@users.noreply.github.com>
2021-09-10 21:35:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 
  <h1>Ratchata techapatamanon<h1>
 
-<iframe src = https://thingspeak.com/channels/1458407/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+<iframe src = https://thingspeak.com/channels/1458407/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=50  style="height:40%;width:35%;"></iframe>
   <br></br>
- <iframe src = https://thingspeak.com/channels/1458407/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15  style="height:40%;width:35%;"></iframe>
+ <iframe src = https://thingspeak.com/channels/1458407/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=50  style="height:40%;width:35%;"></iframe>
+<br></br>
+<iframe src = https://thingspeak.com/channels/1458407/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=50  style="height:40%;width:35%;"></iframe>
+  <br></br>
+ <iframe src = https://thingspeak.com/channels/1458407/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=50  style="height:40%;width:35%;"></iframe>
 <br></br>
   <iframe src = https://thingspeak.com/channels/1458407/maps/channel_show  style="height:45%;width:35%;"></iframe>
 <html>
@@ -24,9 +28,14 @@ function timedRefresh(timeoutPeriod) {
 
  $humidity = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/1/last.txt');
 $temp = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/2/last.txt');
+  $light = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/3/last.txt');
+$lightcheck = file_get_contents('https://api.thingspeak.com/channels/1458407/fields/4/last.txt');
+
 
  echo "<br> temp is = ".$temp."<br>";
  echo "humidity is = ".$humidity;
+  echo "<br> light is = ".$light."<br>";
+ echo "lightcheck is = ".$lightcheck;
  
 ?>
 
