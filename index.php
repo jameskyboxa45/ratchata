d85920784c2b001fa32920a12f4524701deaf358
jameskyboxa45 <88876153+jameskyboxa45@users.noreply.github.com>
2021-09-10 23:14:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,21 @@
   <html lang="en">
     <head>
       <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-      <title>Web Graph</title>
-      <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.0/dist/chart.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>                                                                                                                            
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>Web Graph</title>
+    <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.0/dist/chart.min.js"></script>
+    
     </head>
 
     <body>
@@ -32,8 +41,8 @@
         <div class="class row">
           <div class="class col-3">
             <div class="class row">
-              <div class="class col-4"><b>Temperature</b></div>
-              <div class="col-8" >
+              <div class="class col-5"><b>Temperature:</b></div>
+              <div class="col-1" >
                 <span id="lastTemperature"></span>
               </div>
                 
@@ -41,24 +50,24 @@
             </div>
 
             <div class="class row">
-              <div class="class col-4"><b>Humidity</b></div>
-              <div class="col-8" >
+              <div class="class col-5"><b>Humidity:</b></div>
+              <div class="col-1" >
                 <span id="lastHumidity"></span>
               </div>
                 
               
             </div>
             <div class="class row">
-              <div class="class col-4"><b>Light</b></div>
-              <div class="col-8" >
+              <div class="class col-5"><b>Light:</b></div>
+              <div class="col-1" >
                 <span id="lastlight"></span>
               </div>
                 
               
             </div>
             <div class="class row">
-              <div class="class col-4"><b>Light check</b></div>
-              <div class="col-8" >
+              <div class="class col-5"><b>Light check:</b></div>
+              <div class="col-1" >
                 <span id="lastlightcheck"></span>
               </div>
                 
@@ -81,6 +90,26 @@
     </body>
     <script>
 
+function loaddata(){
+    let url = "https://api.thingspeak.com/channels/1458407/feeds.json?results=50"
+       $.getJSON(url).done(function(data){
+         console.log(data);
+         let feed = data.feeds;
+         console.log(feed);
+             $("#lastTemperature").text(feed[0].field1 + "c");
+             $("#lastHumidity").text(feed[0].field2 + "%")
+             $("#lastlight").text(feed[0].field3 );
+             $("#lastlightcheck").text(feed[0].field4  );
+             $("#lastUpdate").text(feed[0].created_at);
+             
+             
+
+       })
+       .fail(function(error){
+
+
+       });
+  }
       function showChart(data){
         var ctx=document.getElementById('myChart').getContext('2d');
         var myChart=new Chart(ctx,{
@@ -142,16 +171,11 @@
             let url="https://api.thingspeak.com/channels/1458407/feeds.json?results=50";
             $.getJSON(url)
               .done(function(data){
-                //console.log(data);
                 let feed=data.feeds;
                 let chan=data.channel;
                 console.log(feed);
 
-                const d =new Date(feed[0].created_at);
-                const monthNames=["January","February","March","April","May","June","July"
-                                  ,"August","September","October","November","December"];
-                let dateStr = d.getDate()+" "+monthNames[d.getMonth()]+" "+d.getFullYear();
-                dateStr += " "+d.getHours()+":"+d.getMinutes();
+                
 
               var plot_data=Object();
               var xlabel=[];
@@ -162,8 +186,8 @@
               
               $.each(feed,(k,v)=>{
                   xlabel.push(v.entry_id);
-                  humi.push(v.field1);
-                  temp.push(v.field2);
+                  humi.push(v.field2);
+                  temp.push(v.field1);
                   light.push(v.field3);
                   lightcheck.push(v.field4)
                   console.log(k,humi);
@@ -172,41 +196,36 @@
                 var data=new Object();
                 data.xlabel=xlabel;
                 data.data=temp;
-                data.label=chan.field2;
+                data.label=chan.field1;
                 showChart(data);
 
               
                 var data2=new Object();
                 data2.xlabel=xlabel;
                 data2.data=humi;
-                data2.label=chan.field1;
-                showChart(data2);
+                data2.label=chan.field2;
+                showChart2(data2);
 
                 var data3=new Object();
                 data3.xlabel=xlabel;
                 data3.data=light;
-                data3.label=chan.field1;
-                showChart(data3);
+                data3.label=chan.field3;
+                showChart3(data3);
 
                 var data4=new Object();
-                data2.xlabel=xlabel;
-                data2.data=lightcheck;
-                data2.label=chan.field1;
-                showChart(data4);
+                data4.xlabel=xlabel;
+                data4.data=lightcheck;
+                data4.label=chan.field4;
+                showChart4(data4);
 
 
                 
                 
-                $("#lastTemperature").text(feed[0].field2+" C");
-                $("#lastHumidity").text(feed[0].field1+" %");
-                $("#lastlight").text(feed[0].field3+" C");
-                $("#lastlightcheck").text(feed[0].field4+" %");
-                $("#lastUpdate").text(dateStr);
+                
                 console.log(humi);
                 
               })
-              .fail(function(error){
-              });
+              loaddata();  
         });
       
     </script>
